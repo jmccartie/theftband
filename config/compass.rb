d40c222955d0f850b1219a1b7c2dbdc858ce3f96
Jon McCartie <jon@mccartie.com>
2011-05-22 02:16:35 -0500

    Minify CSS, change compass settings

--- config/compass.rb
+++ config/compass.rb
@@ -3,33 +3,12 @@ require 'ninesixty'
 # This configuration file works with both the Compass command line tool and within Rails.
 # Require any additional compass plugins here.
 project_type = :rails
-
+project_path = Rails.root.to_s if defined?(Rails.root)
 # Set this to the root of your project when deployed:
 http_path = "/"
-
 css_dir = "public/stylesheets"
 sass_dir = "app/assets/sass/"
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
+environment = Compass::AppIntegration::Rails.env
+output_style = :compressed
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-preferred_syntax = :sass
-
-
-# # This configuration file works with both the Compass command line tool and within Rails.
-# # Require any additional compass plugins here.
-# project_type = :rails
-# project_path = Rails.root.to_s if defined?(Rails.root)
-# # Set this to the root of your project when deployed:
-# http_path = "/"
-#
-# environment = Compass::AppIntegration::Rails.env
-# output_style = :compressed
-# # To enable relative paths to assets via compass helper functions. Uncomment:
-# # relative_assets = true
